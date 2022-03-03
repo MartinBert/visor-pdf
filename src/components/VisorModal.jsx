a4b58f65509263a98ecd48a04d0945fe073999d0
Martin Bertello <martinbertello7@gmail.com>
2022-03-03 10:46:47 -0300

    update

--- src/components/VisorModal.jsx
+++ src/components/VisorModal.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Modal from "@mui/material/Modal";
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
+import { PDFDocument } from 'pdf-lib';
+import AWS from 'aws-sdk';
+import AmazonS3URI from 'amazon-s3-uri';
 
 const VisorModal = ({ pdf, visorVisible, setVisorVisible }) => {
+    const [pdfLocalUrl, setPdfLocalUrl] = useState(null);
 
     const style = {
         position: 'absolute',
@@ -18,12 +22,34 @@ const VisorModal = ({ pdf, visorVisible, setVisorVisible }) => {
         p: 4,
     };
 
+    useEffect(() => {
+      if(!pdf) return;
+      const loadPdfInBlob = () => {
+        AWS.config.update({
+          credentials: {
+            accessKeyId: 'AKIA22JCCWWQJAB2T3ME',
+            secretAccessKey: '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'
+          },
+          region: 'us-east-2'
+        })
+        const s3 = new AWS.S3();
+        const {bucket, key} = AmazonS3URI(pdf);
+        s3.getObject({Bucket: bucket, Key: key.replaceAll('+', ' ')}, (error, item) => {
+          if(error){
+            console.log(error)
+          }else{
+            const arrayBuffer = item.Body;
+            const blob = new Blob([arrayBuffer], {type: 'application/pdf'});
+            const pdfUrl = URL.createObjectURL(blob);
+            setPdfLocalUrl(pdfUrl);
+          }
+        })
+      } 
+      loadPdfInBlob();
+    }, [pdf])
+
     const printElementDetails = () => {
-        const processedPdf = document.getElementById("pdf").src;
-        const blob = new Blob([processedPdf], {type : 'application/pdf'})
-        const reader = new FileReader();
-        reader.addEventListener("loadend", (e) => {console.log(e.target.result)})
-        reader.readAsArrayBuffer(blob)
+        
     }
 
   return (
@@ -37,7 +63,7 @@ const VisorModal = ({ pdf, visorVisible, setVisorVisible }) => {
                     item
                     xs={12}
                 >
-                <embed id="pdf" src={pdf} type="application/pdf" style={{width: '100%', height: '800px'}}/>
+                  {(pdfLocalUrl) ? <embed id="pdf" src={pdfLocalUrl} type="application/pdf" style={{width: '100%', height: '800px'}}/> : 'Cargando pdf...'}
                 </Grid>
                 <Grid
                     item
